83726848eaf030d615834c87d5c6429a469cbec5
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-26 16:41:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,9 +72,18 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   @output <<-TEXT
-  :winter 
-  end
-  
+  Winter:
+    Christmas: Lights, Wreath
+    New Years: Party Hats
+  Summer:
+    Fourth Of July: Fireworks, BBQ
+  Fall:
+    Thanksgiving: Turkey
+  Spring:
+    Memorial Day: BBQ
+TEXT
+end
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
